4a956155a57f33a27f87441f918fc9cc17b00693
alexis <alexispaniagua1@gmail.com>
2024-11-06 03:38:14 +0100

    Primera prueba

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//PUNTO UNO
+const opciones = ['Papel', 'Tijera', 'Piedra'];
+
+//PUNTO DOS Y TRES
+let puntosuser = 0;
+let puntospc = 0;
+
+//PUNTO CUATRO
+const botones = document.querySelectorAll('boton-jugada');
+const resultDiv = document.getElementById('resultados');
+const contadorUsuario = document.getElementById('contador-usuario');
+const contadorOrdenador = document.getElementById('contador-ordenador');
+
+botones.forEach(boton => {
+    boton.addEventListener('click', () => {
+        
+    })
+})
