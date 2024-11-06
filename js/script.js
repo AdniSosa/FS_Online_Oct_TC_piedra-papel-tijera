cc70deac5d98559e19fc7740cba3235c578eb073
alexis <alexispaniagua1@gmail.com>
2024-11-06 12:28:48 +0100

    Cambios nuevos

--- js/script.js
+++ js/script.js
@@ -6,13 +6,18 @@ let puntosuser = 0;
 let puntospc = 0;
 
 //PUNTO CUATRO
-const botones = document.querySelectorAll('boton-jugada');
+const botones = document.querySelectorAll('.boton-jugada');
 const resultDiv = document.getElementById('resultados');
 const contadorUsuario = document.getElementById('contador-usuario');
 const contadorOrdenador = document.getElementById('contador-ordenador');
 
+console.log(botones);
+
 botones.forEach(boton => {
     boton.addEventListener('click', () => {
-        
+        const jugadaUsuario = boton.dataset.jugada;
+        const jugadaOrdenador = Math.floor(Math.random() * 3);
+        console.log(jugadaOrdenador);
     })
 })
+
