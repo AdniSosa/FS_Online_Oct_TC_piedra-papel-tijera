4f1a88baf3799d39d0f31f509d8345706cb4ab12
Adniloet Sosa <adni.sori@gmail.com>
2024-11-07 11:03:20 +0100

    JavaScrit terminado y CSS actualizado

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 const opciones = ['piedra', 'papel', 'tijera'];
 
 //PUNTO DOS Y TRES
-let puntosuser = 0;
-let puntospc = 0;
+let puntosUser = 0;
+let puntosPc = 0;
 
 //PUNTO CUATRO
 const botones = document.querySelectorAll('.boton-jugada');
@@ -11,26 +11,27 @@ const resultDiv = document.getElementById('resultados');
 const contadorUsuario = document.getElementById('contador-usuario');
 const contadorOrdenador = document.getElementById('contador-ordenador');
 
-console.log(botones);
-
-
+//Un forEach para pasar por cada botón, después del evento click se genere la jugada del ordenador,
+//y se muestre un resultado.
 botones.forEach(boton => {
     boton.addEventListener('click', () => {
-        const jugadaUsuario = boton.dataset.jugada;
+        const jugadaUsuario = boton.dataset.jugada; //Se toma el dato del data.
         const jugadaOrdenador = generarJugadaOrdenador();
-        const resultado = obtenerResultado(jugadaUsuario, jugadaOrdenador);
-
+        const resultado = obtenerResultado(jugadaUsuario, jugadaOrdenador); //Se guarda el resultado en esta constante.
+        //console.log(jugadaUsuario);
         mostrarResultado(jugadaUsuario, jugadaOrdenador, resultado);
         actualizarPuntuacion(resultado);
     });
 });
 
-
+//Se toma un número al azar que será el índice en la matriz y elegir
+//jugada del ordenador.
 function generarJugadaOrdenador() {
-    const numeroran = Math.floor(Math.random() * opciones.length);
-    return opciones[numeroran];
+    const ramdomNumb = Math.floor(Math.random() * opciones.length);
+    return opciones[ramdomNumb];
 }
 
+//Se define las condiciones del juego.
 function obtenerResultado(jugadaUsuario, jugadaOrdenador) {
     if (jugadaUsuario === jugadaOrdenador) {
         return 'empate';
@@ -39,29 +40,33 @@ function obtenerResultado(jugadaUsuario, jugadaOrdenador) {
         (jugadaUsuario === 'papel' && jugadaOrdenador === 'piedra') ||
         (jugadaUsuario === 'tijera' && jugadaOrdenador === 'papel')
     ) { 
-        return 'ganaste';
+        return '¡Has ganado!';
     } else {
-        return 'perdiste';
+        return 'Has perdido.';
     }
 }
 
+//En el div se muestra el resultado de cada jugada.
 function mostrarResultado(jugadaUsuario, jugadaOrdenador, resultado) {
     resultDiv.innerHTML = `
         <p>Has elegido: ${jugadaUsuario}</p>
-        <p>El ordeandor ha elegido: ${jugadaUsuario}</p>
+        <p>El ordenador ha elegido: ${jugadaOrdenador}</p>
         <p>Resultado: ${resultado}</p>
     `
         
 }
 
+//Los puntos que se van generando dependiendo del resultado de la jugada.
 function actualizarPuntuacion(resultado) {
-    if (resultado === 'ganaste') {
-        puntosuser++;
-    }else if (resultado === 'perdiste') {
-        puntospc++;
+    if (resultado === '¡Has ganado!') {
+        puntosUser++;
+    }else if (resultado === 'Has perdido.') {
+        puntosPc++;
     }
-    contadorUsuario.textContent = puntosuser;
-    contadorOrdenador.textContent = puntospc;
+    //console.log(puntosUser);
+    //console.log(puntosPc);
+    contadorUsuario.textContent = puntosUser;
+    contadorOrdenador.textContent = puntosPc;
 }
 
 
